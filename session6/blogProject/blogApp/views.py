bcfe2091a68d3a67588059b971f32296d74f9ec9
monator16 <lys34900@naver.com>
2022-04-04 18:00:51 +0900

    sessuon 6 hw

---
+++ session6/blogProject/blogApp/views.py
@@ -0,0 +1,37 @@
+from unicodedata import category
+from django.shortcuts import render, redirect
+from .models import Article
+
+
+# Create your views here.
+def new(request):
+    if request.method == 'POST':
+        print(request.POST)
+        new_article = Article.objects.create(
+            title = request.POST['title'],
+            content = request.POST['content'],
+            category = request.POST['category'],
+            
+                    
+        )
+        return redirect('list')
+
+    return render(request, 'new.html')
+
+
+def list(request):
+    hobbycount= len(Article.objects.filter(category ="hobby"))
+    foodcount= len(Article.objects.filter(category ="food"))
+    programmingcount= len(Article.objects.filter(category ="programming"))
+    return render(request, 'list.html',{'hobbycount':hobbycount, 'foodcount':foodcount, 'programmingcount':programmingcount})
+
+
+def category(request,category_id):
+    articles= Article.objects.filter(id=category_id)
+    return render(request, 'category.html', {'articles': articles})
+
+def detail(request, article_id):
+    article = Article.objects.get(id=article_id)
+    return render(request, 'detail.html', {'article':article})
+
+    
